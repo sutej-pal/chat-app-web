ea558de55419df4b70bff676bf484da949b5e2da
sutej-pal <sutejpal@hotmail.com>
2020-03-29 13:15:50 +0530

    integrated tailwind

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1 class="text-blue-500">This is an about page</h1>
   </div>
 </template>
+
+<style scoped lang="scss">
+  $red: #000000;
+  .about {
+    color: $red;
+  }
+</style>
