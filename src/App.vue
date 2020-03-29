6d7ff9c08675522bcf4d838db215fe79380a7e28
sutej-pal <sutejpal@hotmail.com>
2020-03-29 12:56:47 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,21 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <Chat/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Chat from './components/Chat.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Chat
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
