1b6421a2cc15388b87a871ff2bd1839ee48ad2d6
sutej-pal <sutejpal@hotmail.com>
2020-05-01 15:13:36 +0530

    auto login

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,22 +1,35 @@
 import Vue from 'vue'
 import HttpService from '../../services/http.service'
+import UtilityService from '../../services/utility.service'
+import _ from 'underscore'
 
 export default Vue.extend({
-  data() {
+  data () {
     return {
       formData: {
         email: '',
         password: ''
-      }
+      },
+      isLoggedIn: false
     }
   },
   methods: {
     login () {
       HttpService.post('login', this.formData).then(res => {
         console.log('result', res)
-        localStorage.setItem('userData', JSON.stringify(res.data.data));
-        this.$router.push({ path: '/home' });
+        localStorage.setItem('userData', JSON.stringify(res.data.data))
+        this.$router.push({ path: '/home' })
       })
     }
+  },
+  beforeMount () {
+    const user = UtilityService.getUserData();
+    setTimeout(() => {
+      if (_.isObject(user)) {
+        this.$router.push({ path: '/home' })
+      } else {
+        this.isLoggedIn = true
+      }
+    }, 2000);
   }
 })
